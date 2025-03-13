930714dc908b13c8f18259e688f09e151aa65f2e
César Greppin <Citcatjoe@users.noreply.github.com>
2025-03-13 11:45:40 +0100

    archive

--- js/main.js
+++ js/main.js
@@ -18,101 +18,100 @@
 
 
 
-    console.clear();
+    //console.clear();
 
     
-    const circle2 = document.querySelector(".circle-2");
-    const circle3 = document.querySelector(".circle-3");
-    const circleA = document.querySelector(".circle-a");
-    const circleB = document.querySelector(".circle-b");
-    const circleC = document.querySelector(".circle-c");
-    const circleD = document.querySelector(".circle-d");
+    // const circle2 = document.querySelector(".circle-2");
+    // const circle3 = document.querySelector(".circle-3");
+    // const circleA = document.querySelector(".circle-a");
+    // const circleB = document.querySelector(".circle-b");
+    // const circleC = document.querySelector(".circle-c");
+    // const circleD = document.querySelector(".circle-d");
 
-    const randomX = random(5, 10);
-    const randomY = random(10, 20);
+    // const randomX = random(5, 10);
+    // const randomY = random(10, 20);
 
-    //pour les éléments au second plan
-    const randomX2 = random(3, 5);
-    const randomY2 = random(3, 5);
+    // //pour les éléments au second plan
+    // const randomX2 = random(3, 5);
+    // const randomY2 = random(3, 5);
 
 
-    const randomDelay = random(0, 1);
-    const randomTime = random(3, 5);
-    const randomTime2 = random(5, 10);
-    const randomAngle = random(8, 12);
+    // const randomDelay = random(0, 1);
+    // const randomTime = random(3, 5);
+    // const randomTime2 = random(5, 10);
+    // const randomAngle = random(8, 12);
 
     
 
-    TweenLite.set(circle2, {
-        x: randomX2(-1),
-        y: randomX2(1),
-        rotation: randomAngle(-1)
-    });
-    moveX2(circle2, 1);
-    moveY2(circle2, -1);
-    rotate(circle2, 1);
+    // TweenLite.set(circle2, {
+    //     x: randomX2(-1),
+    //     y: randomX2(1),
+    //     rotation: randomAngle(-1)
+    // });
+    // moveX2(circle2, 1);
+    // moveY2(circle2, -1);
+    // rotate(circle2, 1);
 
-    TweenLite.set(circle3, {
-        x: randomX2(-1),
-        y: randomX2(1),
-        rotation: randomAngle(-1)
-    });
-    moveX2(circle3, 1);
-    moveY2(circle3, -1);
-    rotate(circle3, 1);
+    // TweenLite.set(circle3, {
+    //     x: randomX2(-1),
+    //     y: randomX2(1),
+    //     rotation: randomAngle(-1)
+    // });
+    // moveX2(circle3, 1);
+    // moveY2(circle3, -1);
+    // rotate(circle3, 1);
     
 
-    TweenLite.set(circleA, {
-        x: randomX(-1),
-        y: randomX(1),
-        rotation: randomAngle(-1)
-    });
-    moveX(circleA, 1);
-    moveY(circleA, -1);
-    rotate(circleA, 1);
+    // TweenLite.set(circleA, {
+    //     x: randomX(-1),
+    //     y: randomX(1),
+    //     rotation: randomAngle(-1)
+    // });
+    // moveX(circleA, 1);
+    // moveY(circleA, -1);
+    // rotate(circleA, 1);
 
-    TweenLite.set(circleB, {
-        x: randomX(-1),
-        y: randomX(1),
-        rotation: randomAngle(-1)
-    });
-    moveX(circleB, 1);
-    moveY(circleB, -1);
-    rotate(circleB, 1);
+    // TweenLite.set(circleB, {
+    //     x: randomX(-1),
+    //     y: randomX(1),
+    //     rotation: randomAngle(-1)
+    // });
+    // moveX(circleB, 1);
+    // moveY(circleB, -1);
+    // rotate(circleB, 1);
 
-    TweenLite.set(circleC, {
-        x: randomX(-1),
-        y: randomX(1),
-        rotation: randomAngle(-1)
-    });
-    moveX(circleC, 1);
-    moveY(circleC, -1);
-    rotate(circleC, 1);
+    // TweenLite.set(circleC, {
+    //     x: randomX(-1),
+    //     y: randomX(1),
+    //     rotation: randomAngle(-1)
+    // });
+    // moveX(circleC, 1);
+    // moveY(circleC, -1);
+    // rotate(circleC, 1);
 
-    TweenLite.set(circleD, {
-        x: randomX(-1),
-        y: randomX(1),
-        rotation: randomAngle(-1)
-    });
-    moveX(circleD, 1);
-    moveY(circleD, -1);
-    rotate(circleD, 1);
-
-
+    // TweenLite.set(circleD, {
+    //     x: randomX(-1),
+    //     y: randomX(1),
+    //     rotation: randomAngle(-1)
+    // });
+    // moveX(circleD, 1);
+    // moveY(circleD, -1);
+    // rotate(circleD, 1);
 
 
 
 
-function rotate(target, direction) {
+
+
+// function rotate(target, direction) {
   
-  TweenLite.to(target, randomTime2(), {
-    rotation: randomAngle(direction),
-    // delay: randomDelay(),
-    ease: Sine.easeInOut,
-    onComplete: rotate,
-    onCompleteParams: [target, direction * -1]
-  });
-}
+//   TweenLite.to(target, randomTime2(), {
+//     rotation: randomAngle(direction),
+//     ease: Sine.easeInOut,
+//     onComplete: rotate,
+//     onCompleteParams: [target, direction * -1]
+//   });
+// }
 
 
 
@@ -121,46 +120,46 @@ function rotate(target, direction) {
 
 
 
-function moveX(target, direction) {
+// function moveX(target, direction) {
   
-  TweenLite.to(target, randomTime(), {
-    x: randomX(direction),
-    ease: Sine.easeInOut,
-    onComplete: moveX,
-    onCompleteParams: [target, direction * -1]
-  });
-}
+//   TweenLite.to(target, randomTime(), {
+//     x: randomX(direction),
+//     ease: Sine.easeInOut,
+//     onComplete: moveX,
+//     onCompleteParams: [target, direction * -1]
+//   });
+// }
 
-function moveY(target, direction) {
+// function moveY(target, direction) {
   
-  TweenLite.to(target, randomTime(), {
-    y: randomY(direction),
-    ease: Sine.easeInOut,
-    onComplete: moveY,
-    onCompleteParams: [target, direction * -1]
-  });
-}
+//   TweenLite.to(target, randomTime(), {
+//     y: randomY(direction),
+//     ease: Sine.easeInOut,
+//     onComplete: moveY,
+//     onCompleteParams: [target, direction * -1]
+//   });
+// }
 
 //pour les éléments au second plan
-function moveX2(target, direction) {
+// function moveX2(target, direction) {
   
-    TweenLite.to(target, randomTime(), {
-      x: randomX2(direction),
-      ease: Sine.easeInOut,
-      onComplete: moveX2,
-      onCompleteParams: [target, direction * -1]
-    });
-  }
+//     TweenLite.to(target, randomTime(), {
+//       x: randomX2(direction),
+//       ease: Sine.easeInOut,
+//       onComplete: moveX2,
+//       onCompleteParams: [target, direction * -1]
+//     });
+//   }
   
-  function moveY2(target, direction) {
+//   function moveY2(target, direction) {
     
-    TweenLite.to(target, randomTime(), {
-      y: randomY2(direction),
-      ease: Sine.easeInOut,
-      onComplete: moveY2,
-      onCompleteParams: [target, direction * -1]
-    });
-  }
+//     TweenLite.to(target, randomTime(), {
+//       y: randomY2(direction),
+//       ease: Sine.easeInOut,
+//       onComplete: moveY2,
+//       onCompleteParams: [target, direction * -1]
+//     });
+//   }
 
 
 
